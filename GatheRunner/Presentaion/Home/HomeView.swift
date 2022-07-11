b01488ad7099bb0d33674e36ef322617b83243d6
atlas <potatoartie@gmail.com>
2022-07-11 18:43:51 +0900

    [add] 재사용성을 위한 뷰 설계 및 개발진행중

--- GatheRunner/Presentaion/Home/HomeView.swift
+++ GatheRunner/Presentaion/Home/HomeView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Home View")
+//        Text("Home View")
+        SettingView()
     }
 }
 
--- GatheRunner/Presentaion/Settings/SettingView.swift
+++ GatheRunner/Presentaion/Settings/SettingView.swift
@@ -27,8 +27,8 @@ struct MeasurementView: View {
         VStack {
             Text("측정")
             HStack {
-                TempletView()
-                TempletView()
+                TempletView(type: .indoorOutdoor())
+                TempletView(type: .autoPause())
             }
         }
     }
@@ -39,25 +39,125 @@ struct DetailControllView: View {
         VStack {
             Text("표시 및 음성")
             HStack {
-                TempletView()
-                TempletView()
+                TempletView(type: .audioFeedback())
+                TempletView(type: .countDown())
             }
             HStack {
-                TempletView()
-                TempletView()
+                TempletView(type: .orientation())
+                TempletView(type: .display())
             }
         }
     }
 }
 
 struct TempletView: View {
+//    @State var type: SettingType
+    var viewModel: ViewModel
+    
     var body: some View {
-        Spacer()
-        VStack {
-            Image(systemName: "person.icloud.fill")
-            Text("hi")
-            Text("hi2")
+        HStack {
+            Spacer()
+            VStack {
+                Image(systemName: viewModel.imageIconName)
+                Text(viewModel.status)
+                Text(viewModel.description)
+            }
+            Spacer()
+        }.onTapGesture {
+            print("Pressed")
         }
-        Spacer()
+    }
+    
+    init(type: SettingType){
+        self.viewModel = ViewModel(type: type)
+    }
+}
+
+enum SettingType {
+    case indoorOutdoor(status: String = "Outdoor", description: String = "Indoor/Outdoor", icon:String = "person.icloud.fill", alignment: Alignment = .center , iconImageHidden: Bool = false, statusTextHidden:Bool = false, descriptionTextHidden: Bool = false)
+    case autoPause(status: String = "On", description: String = "Auto-Pause", icon:String = "person.icloud.fill", alignment: Alignment = .center , iconImageHidden: Bool = false, statusTextHidden:Bool = false, descriptionTextHidden: Bool = false)
+    case audioFeedback(status: String = "Portrait", description: String = "On/Female", icon:String = "person.icloud.fill", alignment: Alignment = .center , iconImageHidden: Bool = false, statusTextHidden:Bool = false, descriptionTextHidden: Bool = false)
+    case countDown(status: String = "3 Seconds", description: String = "Countdown", icon:String = "person.icloud.fill", alignment: Alignment = .center , iconImageHidden: Bool = false, statusTextHidden:Bool = false, descriptionTextHidden: Bool = false)
+    case orientation(status: String = "Portrait", description: String = "Orientation", icon:String = "person.icloud.fill", alignment: Alignment = .center , iconImageHidden: Bool = false, statusTextHidden:Bool = false, descriptionTextHidden: Bool = false)
+    case display(status: String = "Run Level Color", description: String = "Display", icon:String = "person.icloud.fill", alignment: Alignment = .center , iconImageHidden: Bool = false, statusTextHidden:Bool = false, descriptionTextHidden: Bool = false)
+}
+
+extension TempletView {
+    class ViewModel {
+        
+        var status: String
+        var description: String
+        var imageIconName: String
+        var alignment: Alignment
+        var iconImageHidden: Bool
+        var statusTextHidden: Bool
+        var descriptionTextHidden: Bool
+        
+        init(type: SettingType){
+            switch type {
+            case .indoorOutdoor(let status, let description, let icon, let alignment, let iconImageHidden, let statusTextHidden, let descriptionTextHidden):
+                self.status = status
+                self.description = description
+                self.imageIconName = icon
+                self.alignment = alignment
+                self.iconImageHidden = iconImageHidden
+                self.statusTextHidden = statusTextHidden
+                self.descriptionTextHidden = descriptionTextHidden
+            case .autoPause(let status, let description, let icon, let alignment, let iconImageHidden, let statusTextHidden, let descriptionTextHidden):
+                self.status = status
+                self.description = description
+                self.imageIconName = icon
+                self.alignment = alignment
+                self.iconImageHidden = iconImageHidden
+                self.statusTextHidden = statusTextHidden
+                self.descriptionTextHidden = descriptionTextHidden
+            case .audioFeedback(let status, let description, let icon, let alignment, let iconImageHidden, let statusTextHidden, let descriptionTextHidden):
+                self.status = status
+                self.description = description
+                self.imageIconName = icon
+                self.alignment = alignment
+                self.iconImageHidden = iconImageHidden
+                self.statusTextHidden = statusTextHidden
+                self.descriptionTextHidden = descriptionTextHidden
+            case .countDown(let status, let description, let icon, let alignment, let iconImageHidden, let statusTextHidden, let descriptionTextHidden):
+                self.status = status
+                self.description = description
+                self.imageIconName = icon
+                self.alignment = alignment
+                self.iconImageHidden = iconImageHidden
+                self.statusTextHidden = statusTextHidden
+                self.descriptionTextHidden = descriptionTextHidden
+            case .orientation(let status, let description, let icon, let alignment, let iconImageHidden, let statusTextHidden, let descriptionTextHidden):
+                self.status = status
+                self.description = description
+                self.imageIconName = icon
+                self.alignment = alignment
+                self.iconImageHidden = iconImageHidden
+                self.statusTextHidden = statusTextHidden
+                self.descriptionTextHidden = descriptionTextHidden
+            case .display(let status, let description, let icon, let alignment, let iconImageHidden, let statusTextHidden, let descriptionTextHidden):
+                self.status = status
+                self.description = description
+                self.imageIconName = icon
+                self.alignment = alignment
+                self.iconImageHidden = iconImageHidden
+                self.statusTextHidden = statusTextHidden
+                self.descriptionTextHidden = descriptionTextHidden
+            }
+            
+        }
+        
+        func getStatus() -> String {
+            return ""
+        }
+        
+        func getDescription(){
+            
+        }
+        
+        func getImageName(){
+            
+        }
+        
     }
 }
