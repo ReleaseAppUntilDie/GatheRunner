d4d18dc590b42e840f50b9a7dc3eec381aa62479
cho <wh5929@gmail.com>
2022-07-21 15:48:32 +0900

    FloatingView의 '목표 설정' 버튼을 탭했을때 표시되는 모달창의 컨텐츠들을 담을 모델과, 모델뷰 추가
    Model : GoalSetting.swift
    ModelView : GoalSettingViewModel.swift
    참조하는 View : FloatingView.swift
    
    GoalSettingViewModel 에서 isButtonSelected Bool 변수를 @Published 하여 GatherRunnerApp.swift 파일에서 하위 객체들에 .environmentObject(GoalSettingViewModel()) 을 전달하며 데이터를 공유하고있습니다.
    
    getGoalSettingArrs 계산 속성을 사용해 goalSetting 배열을 리턴합니다.

---
+++ GatheRunner/Presentaion/Model/GoalSetting.swift
@@ -0,0 +1,14 @@
+//
+//  GoalSetting.swift
+//  GatheRunner
+//
+//  Created by cho on 2022/07/19.
+//
+
+import SwiftUI
+
+struct GoalSetting: Identifiable {
+    let id = UUID()
+    let title: String
+    let tag: Int
+}
