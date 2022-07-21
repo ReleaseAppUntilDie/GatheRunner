3d97b5580330608894448e3c64216c6d1cbf9f71
cho <wh5929@gmail.com>
2022-07-21 15:54:22 +0900

    UI 객체 크기 수정 및 데이터 바인딩 변수 추가, 타이머 추가, 코드 정리 등
    1. UI 객체 크기는 기존 고정 크기에서 -> 기기별 화면 크기에 맞게 동작하도록 현재 화면 크기 * 배율로 지정
    (현재 정확히 UI가 표현되지 않는 문제가 있어 수정 예정)
    
    2. goalButton, selected 바인딩 변수 추가
    
    3. 러닝가이드 아이템 탭뷰가 시간이 지난후에 자동으로 슬라이드되는 기능을 구현하기 위해 타이머 기능 추가
    
    4. radialGradient 구현 코드부 정리

--- GatheRunner/Presentaion/Running/FloatingView.swift
+++ GatheRunner/Presentaion/Running/FloatingView.swift
@@ -8,43 +8,52 @@
 import SwiftUI
 
 struct FloatingView: View {
+    @EnvironmentObject var goalButton : GoalSettingViewModel
+    
     var body: some View {
         VStack(spacing: 10) {
             HStack(alignment: .center, spacing: 30) {
                 Button {
                 } label: {
                     Image(systemName: "gearshape.fill")
-                        .frame(width: 50, height: 50)
-                        .font(Font.system(size: 25)).foregroundColor(Color.black)
+                        .frame(width: UIScreen.main.bounds.width * 0.1154, height: UIScreen.main.bounds.height * 0.0592)
+                        .font(.system(size: 25)).foregroundColor(Color.black)
                         .background(Color.white)
                         .cornerRadius(200)
+                        .shadow(color: Color.gray, radius: 2, x: 3, y: 3)
                 }
                 
                 Button("시작") {
                     
-                }.frame(width: 100, height: 100)
-                    .background(Color.yellow)
-                    .font(.system(size: 27, weight: .black)).foregroundColor(Color.black)
-                    .cornerRadius(200)
+                }
+                .frame(width: UIScreen.main.bounds.width * 0.2820, height: UIScreen.main.bounds.height * 0.1303)
+                .background(Color.yellow)
+                .font(.system(size: 27, weight: .black)).foregroundColor(Color.black)
+                .cornerRadius(200)
                 
                 Button {
                     
                 } label: {
                     Image(systemName: "music.note")
-                        .frame(width: 50, height: 50)
-                        .font(Font.system(size: 25)).foregroundColor(Color.black)
+                        .frame(width: UIScreen.main.bounds.width * 0.1154, height: UIScreen.main.bounds.height * 0.0592)
+                        .font(.system(size: 25)).foregroundColor(Color.black)
                         .background(Color.white)
                         .cornerRadius(200)
+                        .shadow(color: Color.gray, radius: 2, x: 3, y: 3)
                 }
             }
             
             Button("목표 설정") {
-                
-            }.frame(width: 100, height: 40)
-                .font(Font.system(size: 17, weight: .medium)).foregroundColor(Color.black)
-                .background(Color.red)
-                .cornerRadius(50)
-                .padding(10)
+                withAnimation {
+                    goalButton.isButtonSelected = true
+                }
+            }
+            .frame(width: UIScreen.main.bounds.width * 0.26, height: UIScreen.main.bounds.height * 0.07)
+            .font(.system(size: 17, weight: .medium)).foregroundColor(Color.black)
+            .background(Color.red)
+            .cornerRadius(50)
+            .padding(EdgeInsets(top: 10, leading: 10, bottom: 20, trailing: 10))
+            .shadow(color: Color.gray, radius: 2, x: 3, y: 3)
         }
     }
 }
@@ -52,5 +61,6 @@ struct FloatingView: View {
 struct FloatingView_Previews: PreviewProvider {
     static var previews: some View {
         FloatingView()
+            .environmentObject(GoalSettingViewModel())
     }
 }
