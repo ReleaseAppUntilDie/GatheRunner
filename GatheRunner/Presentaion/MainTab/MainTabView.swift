157b3c76eb516f46f53ba0b58876742011652693
cho <wh5929@gmail.com>
2022-07-21 16:58:18 +0900

    메인 탭뷰에서 모달 형식의 BottomSheet 호출 코드추가, 데이터 바인딩 코드 추가
    1. 메인 탭뷰에서 모달 형식의 BottomSheet 호출 코드추가
    -> 메인 탭뷰 위에 뷰를 표시하려면 현재 구성상 메인 탭뷰에서 호출해야 하기 때문.
    
    2. 1번 기능을 구현하기 위해 데이터 바인딩 사용
    
    3. BottomSheet 뷰는 UI 및 기능 수정 예정

--- GatheRunner/Presentaion/MainTab/MainTabView.swift
+++ GatheRunner/Presentaion/MainTab/MainTabView.swift
@@ -9,13 +9,43 @@ import SwiftUI
 
 struct MainTabView: View {
     @State private var selectedTab = 1
+    @EnvironmentObject var goalButton: GoalSettingViewModel
     
     var body: some View {
-        TabView(selection: $selectedTab) {
-            ForEach(tabItems) { $0.body }
+        ZStack {
+            TabView(selection: $selectedTab) {
+                ForEach(tabItems) { $0.body }
+            }
+            .accentColor(.black)
+            
+            VStack {
+                Spacer()
+                if goalButton.isButtonSelected {
+                    BottomSheet()
+                        .frame(width: UIScreen.main.bounds.width, height: 220)
+                        .transition(.move(edge: .bottom))
+                }
+            }
         }
-        .accentColor(.black)
-        
+    }
+}
+
+struct BottomSheet: View {
+    private var getGoalSettingArrs = GoalSettingViewModel().getGoalSettingArrs
+    @EnvironmentObject var goalButton: GoalSettingViewModel
+    var body: some View {
+        VStack(alignment: .leading) {
+            ForEach(getGoalSettingArrs.indices) { index in
+                Text(getGoalSettingArrs[index].title)
+                if getGoalSettingArrs.count - 1 > index {
+                    Divider()
+                }
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.white)
+        .mask(RoundedTopCorner(radius: 30))
+        .ignoresSafeArea()
     }
 }
 
@@ -28,5 +58,6 @@ extension MainTabView {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         MainTabView()
+            .environmentObject(GoalSettingViewModel())
     }
 }
