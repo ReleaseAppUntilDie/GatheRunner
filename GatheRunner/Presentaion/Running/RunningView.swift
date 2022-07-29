35b99957a138ca1bfa9d4a3973f8345efe198e8f
cho <wh5929@gmail.com>
2022-07-29 16:39:26 +0900

    기능 추가 및 UI 객체 기기 화면 크기별 동적 변경
    UI 객체 사이즈 기기 화면 크기 별로 동적으로 수정되도록 변경 -> 수정 필요
    
    FloatingView 의 ‘목표 설정' 버튼을 탭했을때 표시되는 모달형태의 뷰가 MainTabView에서 표시되도록 기능 추가
    
    FloatingView에서 목표 설정 버튼을 눌렀을때 다른 뷰에서 화면 전환, 뷰 호출등의 기능을 구현하기 위해 Environmentobject 사용
    runningGuideTabView
    
    runningGuideTabView 아래에 페이지 상태 표시 바 추가
    runningGuideTabView 가 사용자의 입력이 없을때 시간이 지난후 자동으로 슬라이드 되는 기능 추가
    runningGuideTabView 가 사용자의 입력이 있을 경우 자동 슬라이드가 멈추는 기능 추가

--- GatheRunner/Presentaion/Running/RunningView.swift
+++ GatheRunner/Presentaion/Running/RunningView.swift
@@ -8,63 +8,68 @@
 import SwiftUI
 
 struct RunningView: View {
-    @State var justStartButtonIsSelected: Bool = true
-    @State var runningGuideButtonIsSelected: Bool = false
+    @State private var justStartButtonIsSelected: Bool = true
+    @State private var runningGuideButtonIsSelected: Bool = false
+    @ObservedObject var goalButton = GoalSettingViewModel.shared
+    @State var selected: Int = 0
     
     var body: some View {
         ZStack {
-            VStack(alignment: .leading) {
-                HStack(alignment: .top) {
-                    Text("Header View")
-                }
-                .frame(maxWidth: .infinity, maxHeight: 120)
-                
-                HStack {
-                    Button("바로 시작") {
-                        withAnimation {
-                            if !justStartButtonIsSelected && runningGuideButtonIsSelected {
-                                justStartButtonIsSelected.toggle()
-                                runningGuideButtonIsSelected.toggle()
-                            }
-                        }
-                    }
-                    .frame(width: 100, height: 10)
-                    .foregroundColor(justStartButtonIsSelected ? Color.black : Color.gray)
-                    .padding(4)
-     
-                    Button("러닝 가이드") {
-                        withAnimation {
-                            if justStartButtonIsSelected && !runningGuideButtonIsSelected {
-                                runningGuideButtonIsSelected.toggle()
-                                justStartButtonIsSelected.toggle()
-                            }
-                        }
-                    }.foregroundColor(runningGuideButtonIsSelected ? Color.black : Color.gray)
-                }
-                .zIndex(1)
-                .frame(width: 400, height: 30, alignment: .leading)
-                    .font(Font.system(size: 14))
-
-                VStack {
-                    if justStartButtonIsSelected {
-                        JustStartView()
-                            .transition(.move(edge: .leading))
-                    }
-                    
-                    if runningGuideButtonIsSelected {
-                        RunningGuideView()
-                            .transition(.move(edge: .trailing))
-                    }
-                }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        VStack(alignment: .center) {
+            Text("Header View")
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.1421)
+            HStack {
+                commonBtn(title: "바로 시작", fontSize: 14, isToggled: $justStartButtonIsSelected, isOtherToggled: $runningGuideButtonIsSelected)
+                    .padding(15)
+                commonBtn(title: "러닝 가이드", fontSize: 14, isToggled: $runningGuideButtonIsSelected, isOtherToggled: $justStartButtonIsSelected)
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-                .ignoresSafeArea(edges: .top)
+            .frame(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height * 0.0355, alignment: .leading)
+            .frame(maxWidth: .infinity, maxHeight: 30, alignment: .leading)
+            
+            if justStartButtonIsSelected {
+                JustStartView(selected: $selected)
+                    .transition(.move(edge: .leading))
+            }
+
+            if runningGuideButtonIsSelected {
+                RunningGuideView()
+                    .transition(.move(edge: .trailing))
+            }
+        }.overlay {
+            Color.black.opacity(goalButton.isButtonSelected ? 0.4 : 0)
+        }
+        .onTapGesture {
+            withAnimation {
+                goalButton.isButtonSelected = false
+            }
+        }.ignoresSafeArea(edges: .top)
         }
     }
 }
 
+struct commonBtn: View {
+    var title: String
+    var fontSize: Int
+    @Binding var isToggled: Bool
+    @Binding var isOtherToggled: Bool
+    
+    var body: some View {
+        Button(title) {
+            withAnimation {
+                if !isToggled {
+                    self.isToggled.toggle()
+                    self.isOtherToggled.toggle()
+                }
+            }
+        }
+        .foregroundColor(isToggled ? Color.black : Color.gray)
+        .font(.system(size: CGFloat(fontSize)))
+    }
+}
+
+
 struct RunningView_Previews: PreviewProvider {
     static var previews: some View {
-        RunningView()
+        RunningView(selected: 0).environmentObject(GoalSettingViewModel())
     }
 }
