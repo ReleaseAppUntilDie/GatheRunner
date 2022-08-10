058c97b4c960127cd804e04fd9cb8500e2f95994
hanjaesueng <fx12wkd@naver.com>
2022-08-10 17:00:11 +0900

    add TimeUnitTable

--- GatheRunner/Presentaion/Activity/GraphView.swift
+++ GatheRunner/Presentaion/Activity/GraphView.swift
@@ -12,81 +12,138 @@ fileprivate enum TimeUnit {
 }
 
 struct GraphView: View {
-    @State private var weekSelect = true
-    @State private var monthSelect = false
-    @State private var yearSelect = false
-    @State private var wholeSelect = false
+    
+    @State private var selectedTimeUnit : TimeUnit = .week
+    var unitIndex : Double {
+        switch selectedTimeUnit {
+        case .week: return 0
+        case .month: return 1
+        case .year: return 2
+        case .whole: return 3
+        }
+    }
+    
     
     var body: some View {
         VStack {
-            ZStack {
-                RoundedRectangle(cornerRadius: UIScreen.getHeightby(ratio: 0.02))
-                    .strokeBorder(Color(uiColor: .systemGray4),lineWidth: 2)
-                    .frame(width: UIScreen.getWidthby(ratio: 0.8), height: UIScreen.getHeightby(ratio: 0.04))
-              
-                HStack(alignment: .verticalCustom){
-                    Component(isSelected: $weekSelect, title: "주")
-                    Component(isSelected: $monthSelect, title: "월")
-                    Component(isSelected: $yearSelect, title: "년")
-                    Component(isSelected: $wholeSelect, title: "전체")
-                }
-                .frame(width: UIScreen.getWidthby(ratio: 0.8), height: UIScreen.getHeightby(ratio: 0.04))
+            TimeUnitsTable(curTimeUnit: $selectedTimeUnit,unitIndex: unitIndex)
                 .onTouch(type: .started) { point in
-                    
-                    if point.x < UIScreen.getWidthby(ratio: 0.2) {
-                        weekSelect = true
-                        monthSelect = false
-                        yearSelect = false
-                        wholeSelect = false
-                        print("주")
-                    } else if point.x >= UIScreen.getWidthby(ratio: 0.2) && point.x < UIScreen.getWidthby(ratio: 0.4) {
-                        weekSelect = false
-                        monthSelect = true
-                        yearSelect = false
-                        wholeSelect = false
-                        print("월")
-                    } else if point.x >= UIScreen.getWidthby(ratio: 0.4) && point.x < UIScreen.getWidthby(ratio: 0.6) {
-                        weekSelect = false
-                        monthSelect = false
-                        yearSelect = true
-                        wholeSelect = false
-                        print("년")
-                    } else if point.x >= UIScreen.getWidthby(ratio: 0.6) {
-                        weekSelect = false
-                        monthSelect = false
-                        yearSelect = false
-                        wholeSelect = true
-                        print("전체")
+                    withAnimation {
+                        if point.x < UIScreen.getWidthby(ratio: 0.2) {
+                            selectedTimeUnit = .week
+                        } else if point.x >= UIScreen.getWidthby(ratio: 0.2)&&point.x<UIScreen.getWidthby(ratio: 0.4){
+                            selectedTimeUnit = .month
+                        } else if point.x >= UIScreen.getWidthby(ratio: 0.4)&&point.x<UIScreen.getWidthby(ratio: 0.6) {
+                            selectedTimeUnit = .year
+                        } else {
+                            selectedTimeUnit = .whole
+                        }
                     }
-                    
                 }
-            }
-        }
-    }
-    
-    
-    struct Component : View {
-        @Binding var isSelected : Bool
-        let title : String
-        var body : some View {
-            ZStack(alignment:.center) {
-                if isSelected {
-                    RoundedRectangle(cornerRadius: UIScreen.getHeightby(ratio: 0.02))
-                        .fill(Color.green)
-                        .frame(width: UIScreen.getWidthby(ratio: 0.15), height: UIScreen.getHeightby(ratio: 0.04))
-                }
-                Text(title)
-                    .foregroundColor(isSelected ? .black : .init(uiColor: .systemGray2))
-                    .frame(width: UIScreen.getWidthby(ratio: 0.20))
-            }
+            
+            SimplifiedStatistics(selectedTimeUnit: .week)
         }
     }
 }
+
+struct TimeUnitsTable : View{
+    
+    @Binding fileprivate var curTimeUnit : TimeUnit
+    let unitIndex : Double
+    
+    var body: some View{
+        ZStack {
+            RoundedRectangle(cornerRadius: UIScreen.getHeightby(ratio: 0.02))
+                .strokeBorder(Color(uiColor: .systemGray4),lineWidth: 2)
+                .frame(width: UIScreen.getWidthby(ratio: 0.8), height: UIScreen.getHeightby(ratio: 0.04))
+            
+            ZStack {
+                RoundedRectangle(cornerRadius: UIScreen.getHeightby(ratio: 0.02))
+                    .fill(Color.green)
+                    .frame(width: UIScreen.getWidthby(ratio: 0.2), height: UIScreen.getHeightby(ratio: 0.04))
+                    .offset(x: -UIScreen.getWidthby(ratio: 0.3) + UIScreen.getWidthby(ratio: 0.2 * unitIndex))
+                HStack(spacing:0){
+                    TimeUnitComponent(selectedTimeUnit: $curTimeUnit, timeUnit: .week, title: "주")
+                        .frame(width: UIScreen.getWidthby(ratio: 0.2))
+                    TimeUnitComponent(selectedTimeUnit: $curTimeUnit, timeUnit: .month, title: "월")
+                        .frame(width: UIScreen.getWidthby(ratio: 0.2))
+                    TimeUnitComponent(selectedTimeUnit: $curTimeUnit, timeUnit: .year, title: "년")
+                        .frame(width: UIScreen.getWidthby(ratio: 0.2))
+                    TimeUnitComponent(selectedTimeUnit: $curTimeUnit, timeUnit: .whole, title: "전체")
+                        .frame(width: UIScreen.getWidthby(ratio: 0.2))
+                }
+            }
+            
+        }
+    }
+    struct TimeUnitComponent : View {
+        @Binding fileprivate var selectedTimeUnit : TimeUnit
+        fileprivate let timeUnit : TimeUnit
+        let title : String
+        var body : some View {
+            Text(title)
+                .foregroundColor(selectedTimeUnit == timeUnit ? .black : .init(uiColor: .systemGray2))
+        }
+    }
+}
+
+struct SimplifiedStatistics: View{
+    
+    fileprivate let selectedTimeUnit : TimeUnit
+    
+    var body: some View{
+        VStack(alignment: .leading,spacing: UIScreen.getHeightby(ratio: 0.1)) {
+            
+        }
+        .onAppear {
+            print(beforeTwoWeeks())
+            print(beforeThreeWeeks())
+        }
+    }
+    
+    func beforeTwoWeeks() -> (firstDay:Int?,firstMonth:Int?,lastDay:Int?,lastMonth:Int?){
+        let date = Date()
+        guard let weekDay = Calendar.current.dateComponents([.weekday], from: date).weekday else {
+            return (nil,nil,nil,nil)
+        }
+        
+        guard let beforeTwoWeeks = Calendar.current.date(byAdding: .day, value: -(14+(weekDay-2)), to: date) else {
+            return (nil,nil,nil,nil)
+        }
+        guard let beforeOneWeek = Calendar.current.date(byAdding: .day, value: -(7+(weekDay-1)), to: date) else {
+            return (nil,nil,nil,nil)
+        }
+        
+        let firstDateAndMonth = Calendar.current.dateComponents([.month,.day], from: beforeTwoWeeks)
+        let lastDateAndMonth = Calendar.current.dateComponents([.month,.day], from: beforeOneWeek)
+        return (firstDateAndMonth.day,firstDateAndMonth.month,lastDateAndMonth.day,lastDateAndMonth.month)
+    }
+    
+    func beforeThreeWeeks() -> (firstDay:Int?,firstMonth:Int?,lastDay:Int?,lastMonth:Int?){
+        let date = Date()
+        guard let weekDay = Calendar.current.dateComponents([.weekday], from: date).weekday else {
+            return (nil,nil,nil,nil)
+        }
+        
+        guard let beforeTwoWeeks = Calendar.current.date(byAdding: .day, value: -(21+(weekDay-2)), to: date) else {
+            return (nil,nil,nil,nil)
+        }
+        guard let beforeOneWeek = Calendar.current.date(byAdding: .day, value: -(14+(weekDay-1)), to: date) else {
+            return (nil,nil,nil,nil)
+        }
+        
+        let firstDateAndMonth = Calendar.current.dateComponents([.month,.day], from: beforeTwoWeeks)
+        let lastDateAndMonth = Calendar.current.dateComponents([.month,.day], from: beforeOneWeek)
+        return (firstDateAndMonth.day,firstDateAndMonth.month,lastDateAndMonth.day,lastDateAndMonth.month)
+    }
+}
+
 
 struct GraphView_Previews: PreviewProvider {
     static var previews: some View {
         GraphView()
     }
 }
+
 
 
