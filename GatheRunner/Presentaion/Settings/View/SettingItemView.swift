9f3dbe36e39cece46c6010bf26a3a360bd2d1196
kdh <ejxlabvlzhxm@naver.com>
2022-08-25 16:36:29 +0900

    FIX: conflict 에러 수정

--- GatheRunner/Presentaion/Settings/View/SettingItemView.swift
+++ GatheRunner/Presentaion/Settings/View/SettingItemView.swift
@@ -7,15 +7,9 @@
 
 import SwiftUI
 
-<<<<<<< refs/remotes/origin/develop:GatheRunner/Presentaion/Settings/View/MeasurementView.swift
-// MARK: - MeasurementView
-
-struct MeasurementView: View {
-=======
 // MARK: - SettingItemView
 
 struct SettingItemView: View {
->>>>>>> FEAT: 페이스 및 토글버튼 구현:GatheRunner/Presentaion/Settings/View/SettingItemView.swift
     var body: some View {
         VStack(spacing: 10) {
             HStack {
@@ -35,15 +29,9 @@ struct SettingItemView: View {
     }
 }
 
-<<<<<<< refs/remotes/origin/develop:GatheRunner/Presentaion/Settings/View/MeasurementView.swift
-// MARK: - MeasurementView_Previews
-
-struct MeasurementView_Previews: PreviewProvider {
-=======
 // MARK: - SettingItemView_Previews
 
 struct SettingItemView_Previews: PreviewProvider {
->>>>>>> FEAT: 페이스 및 토글버튼 구현:GatheRunner/Presentaion/Settings/View/SettingItemView.swift
     static var previews: some View {
         SettingItemView()
     }
