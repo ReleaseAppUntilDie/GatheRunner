8aa0b4d437a03f4a8b202fc76fc5dfbf7c53756a
cho <wh5929@gmail.com>
2022-08-25 15:11:30 +0900

    firebase

--- GatheRunner/GatheRunnerApp.swift
+++ GatheRunner/GatheRunnerApp.swift
@@ -5,10 +5,23 @@
 //  Created by 김동현 on 2022/06/29.
 //
 
+
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct GatheRunnerApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             MainTabView()
