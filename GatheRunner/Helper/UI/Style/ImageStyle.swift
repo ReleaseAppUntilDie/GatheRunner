2611318d494e37d0477064b63bd4b59c4ca1ed8b
kdh <ejxlabvlzhxm@naver.com>
2022-09-13 20:23:13 +0900

    FIX: 파라미터 오타 수정

--- GatheRunner/Helper/UI/Style/ImageStyle.swift
+++ GatheRunner/Helper/UI/Style/ImageStyle.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 extension Image {
-    func asIconStyle(withMaxWidth maxWidth: CGFloat = 20, withMaxHeight maxHeigh: CGFloat = 20) -> some View {
+    func asIconStyle(withMaxWidth maxWidth: CGFloat = 20, withMaxHeight maxHeight: CGFloat = 20) -> some View {
         resizable()
             .aspectRatio(contentMode: .fit)
-            .frame(width: maxWidth, height: maxHeigh)
+            .frame(width: maxWidth, height: maxHeight)
             .foregroundColor(.white)
     }
 }
