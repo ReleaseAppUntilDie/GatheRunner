71293cf6c03c0af2ba241b5ff79c4145ac9bd77a
jshan <jshan113@hyundai-autoever.com>
2022-10-25 08:27:59 +0900

    [Fix, Picker View에서 Item 선택하지 않고 선택 누를 경우 빈문자열로 output 생성]
    GraphBottomSheetView의 selected 변수 초기화

--- GatheRunner/Presentaion/Activity/Picker/GraphBottomSheetView.swift
+++ GatheRunner/Presentaion/Activity/Picker/GraphBottomSheetView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 // MARK: - GraphBottomSheetView
 
 struct GraphBottomSheetView: View {
-
+    
     // MARK: Lifecycle
-
+    
     init(
         viewModel: GraphViewModel,
         show: Binding<Bool>,
@@ -21,21 +21,21 @@ struct GraphBottomSheetView: View {
         self.viewModel = viewModel
         _show = show
         _selectedTimeUnit = selectedTimeUnit
-
+        
         let current = Calendar.current.dateComponents([.year,.month], from: Date())
         selectedYear = current.year!
         selectedMonth = current.month!
     }
-
+    
     // MARK: Internal
-
+    
     @ObservedObject var viewModel: GraphViewModel
-    @State var selected = ""
+    @State var selected = "이번주"
     @State var selectedMonth: Int
     @State var selectedYear: Int
     @Binding var show: Bool
     @Binding var selectedTimeUnit: TimeUnit
-
+    
     var body: some View {
         ZStack(alignment: .bottom) {
             Color.black.opacity(show ? 0.5 : 0)
@@ -45,15 +45,15 @@ struct GraphBottomSheetView: View {
                         show.toggle()
                     }
                 }
-
-            VStack {
+            
+            LazyVStack {
                 PickerView(
                     viewModel: viewModel,
                     selected: $selected,
                     selectedMonth: $selectedMonth,
                     selectedYear: $selectedYear,
                     isMonth: selectedTimeUnit == .month)
-                    .padding()
+                .padding()
                 Button {
                     confirmButtonAction()
                 } label: {
@@ -70,7 +70,7 @@ struct GraphBottomSheetView: View {
             .cornerRadius(16, corners: [.topLeft,.topRight])
         }
     }
-
+    
     func confirmButtonAction() {
         viewModel.updateSelected(selectedStr: selected, selectedYear: selectedYear, selectedMonth: selectedMonth)
         viewModel.fetchData()
@@ -83,40 +83,38 @@ struct GraphBottomSheetView: View {
 // MARK: - PickerView
 
 struct PickerView: View {
-
+    
     @ObservedObject var viewModel: GraphViewModel
     @Binding var selected: String
     @Binding var selectedMonth: Int
     @Binding var selectedYear: Int
     var isMonth: Bool
-
+    
     var body: some View {
-        LazyVStack {
-            if !isMonth {
-                Picker("Choose period", selection: $selected) {
-                    ForEach(viewModel.pickerItemList,id: \.self) {
-                        Text($0)
+        if !isMonth {
+            Picker("Choose period", selection: $selected) {
+                ForEach(viewModel.pickerItemList,id: \.self) {
+                    Text($0)
+                }
+            }
+            .pickerStyle(.wheel)
+        } else {
+            HStack(spacing: 0) {
+                Picker("Choose year", selection: $selectedYear) {
+                    ForEach(viewModel.pickerItemListInMonth.0,id: \.self) {
+                        Text(verbatim: "\($0)년")
                     }
                 }
+                .frame(width: UIScreen.getWidthby(ratio: 0.5))
                 .pickerStyle(.wheel)
-            } else {
-                HStack(spacing: 0) {
-                    Picker("Choose year", selection: $selectedYear) {
-                        ForEach(viewModel.pickerItemListInMonth.0,id: \.self) {
-                            Text(verbatim: "\($0)년")
-                        }
+                
+                Picker("Choose month", selection: $selectedMonth) {
+                    ForEach(viewModel.pickerItemListInMonth.1,id: \.self) {
+                        Text("\($0)월")
                     }
-                    .frame(width: UIScreen.getWidthby(ratio: 0.5))
-                    .pickerStyle(.wheel)
-
-                    Picker("Choose month", selection: $selectedMonth) {
-                        ForEach(viewModel.pickerItemListInMonth.1,id: \.self) {
-                            Text("\($0)월")
-                        }
-                    }
-                    .frame(width: UIScreen.getWidthby(ratio: 0.5))
-                    .pickerStyle(.wheel)
                 }
+                .frame(width: UIScreen.getWidthby(ratio: 0.5))
+                .pickerStyle(.wheel)
             }
         }
     }
