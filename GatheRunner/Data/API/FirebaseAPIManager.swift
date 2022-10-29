2356412e35671837d4ffeb2d8f34f643a2d5ae75
kdh <ejxlabvlzhxm@naver.com>
2022-10-29 21:11:54 +0900

    REFACTOR: 1.getDocuments의 as 파라미터 타입 수정 2. DATA/Helper 루트 추가 및 Utils,Helper 경로의 파일 구성 변경
    FEAT:
    1. getDocument 추가
    2. getDocument에 필요한 Query+map 메서드 추가

--- GatheRunner/Data/API/FirebaseAPIManager.swift
+++ GatheRunner/Data/API/FirebaseAPIManager.swift
@@ -15,11 +15,21 @@ class FirebaseAPIManager {
     // MARK: Internal
 
     static let shared = FirebaseAPIManager()
+    
+    func getDocument<D: Decodable>(
+        collection: CollectionOption,
+        queries: [QueryOption]? = nil,
+        as type: D.Type) -> AnyPublisher<[D], Error>
+    {
+        db.collection(collection.rawValue)
+            .addQueries(queries)
+            .map(decodeWith: type)
+    }
 
     func getDocuments<D: Decodable>(
         collection: CollectionOption,
         queries: [QueryOption]? = nil,
-        as type: [D].Type) -> AnyPublisher<[D], Error>
+        as type: D.Type) -> AnyPublisher<[D], Error>
     {
         db.collection(collection.rawValue)
             .addQueries(queries)
