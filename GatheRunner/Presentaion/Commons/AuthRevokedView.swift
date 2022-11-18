48207741be8d840ea96c89935690efd14ff63902
kdh <ejxlabvlzhxm@naver.com>
2022-11-19 01:49:03 +0900

    REFACTOR: 1. AppDI 변경사항 적용(변경내용은 해당PR에서 설명) 2. AppDI preview static 프로퍼티 추가 3. main - AppDI 추가, 초기화 로직 변경, 환경변수로 전달 4. MainTabView, ActivityView, RunningView, HeaderView - AppDI 환경변수 추가 및 전달하는 로직 추가 5. AuthRevokedView 추가 6. HeaderView - sheet 연산자로 AuthRevokedView 호출하는 방식으로 변경

---
+++ GatheRunner/Presentaion/Commons/AuthRevokedView.swift
@@ -0,0 +1,40 @@
+//
+//  AuthRevokedView.swift
+//  GatheRunner
+//
+//  Created by 김동현 on 2022/11/19.
+//
+
+import SwiftUI
+
+struct AuthRevokedView: View {
+    private enum Size {
+        static let spacing: CGFloat = 20
+    }
+
+    private enum Label {
+        static let userInfoSetting = "회원정보 설정"
+        static let signOut = "로그아웃"
+        static let deleteUser = "회원탈퇴"
+        static let cancle = "취소"
+    }
+    
+    @Binding var isShownSheet: Bool
+    @StateObject var viewModel: AuthenticationViewModel
+    
+    var body: some View {
+        VStack(spacing: Size.spacing) {
+            Button(Label.signOut) {
+                viewModel.signOut()
+                isShownSheet.toggle()
+            }
+            Button(Label.deleteUser) {
+                viewModel.deleteUser()
+                isShownSheet.toggle()
+            }
+            Button(Label.cancle) {
+                isShownSheet.toggle()
+            }
+        }
+    }
+}
