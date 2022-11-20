3f25cc5ca01ce28c91a0da21aa02ca5f1070a3d1
Cho <wh5929@gmail.com>
2022-11-20 19:43:32 +0900

    ### PR Review 내용 반영
    1. 클래스명 변경 WeatherInfoViewModel -> PrepareRunViewModel
    2. 구조체명 변경 JustStartView -> PrepareRunView (운동 준비 뷰)
    	2-1. View 타입 변수명 변경 -> weatherInfoDisplay -> workoutIndexView
    	2-2. PrepareRunViewModel 이 다중호출 되는것을 막기 위해 PrepareRunView 의 onAppear Modifier 에서 PrepareRunViewModel의 getWorkoutIndex 함수(firestore에서 fetch한 데이터를 String으로 가공하여 받아오는 함수)를 호출하도록 함
    	2-3. 매직넘버를 네임스페이스 상수를 사용하는것으로 변경
    	2-4. PrepareRunView 내부에서 처리하던 텍스트처리를 PrepareRunViewModel 에서 처리하도록 변경
    
    3. 불필요한 주석 코드 제거

--- GatheRunner/Presentaion/Running/Views/PrepareRunView/BottomButtonView.swift
+++ GatheRunner/Presentaion/Running/Views/PrepareRunView/BottomButtonView.swift
@@ -65,12 +65,6 @@ struct StartBtn: View {
     }
 }
 
-//struct WeatherInfoBtn: View {
-//    var body: some View {
-//
-//    }
-//}
-
 extension StartBtn {
     private enum Size {
         static let startBtnWidthRatio = 0.2820
