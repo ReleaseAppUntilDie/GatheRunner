f4784d01723d3dff1991201e0d3ab3953a615cdf
kdh <ejxlabvlzhxm@naver.com>
2022-11-30 03:53:48 +0900

    REFACTOR: LoadableView UI로직 가독성 개선

--- GatheRunner/Helper/UI/Modifier/LoadableView.swift
+++ GatheRunner/Helper/UI/Modifier/LoadableView.swift
@@ -10,21 +10,15 @@ import SwiftUI
 typealias ErrorOption = (errorMessage: String, retryAction: () -> Void)
 
 struct LoadableView: ViewModifier {
-    
     @Binding var fetchStatus: FetchStatus
+    
     var errorOption: ErrorOption?
     
     func body(content: Content) -> some View {
         Group {
-            switch fetchStatus {
-            case .fetching: ActivityIndicatorView()
-            case .failure:
-                if let errorOption = errorOption {
-                    ErrorView(errorMessage: errorOption.errorMessage, retryAction: errorOption.retryAction)
-                } else {
-                    content
-                }
-                
+            switch (fetchStatus, errorOption) {
+            case (.fetching, _): ActivityIndicatorView()
+            case (.failure, .some(let option)): ErrorView(errorMessage: option.errorMessage, retryAction: option.retryAction)
             default: content
             }
         }
