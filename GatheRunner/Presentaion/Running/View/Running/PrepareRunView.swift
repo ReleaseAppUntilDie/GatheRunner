a55616ccd3fa9e2e9961d66526d991518bb2b60f
kdh <ejxlabvlzhxm@naver.com>
2022-12-05 15:49:13 +0900

    REFACTOR: UI 로직
    1. 불필요한 타입 제거 (BottomButtonView 제거 후 프로퍼티로 변경)
    2. 폴더 구조 및 파일 정리
    3. PrepareRunViewModel 네임스페이스 추가 및 적용

---
+++ GatheRunner/Presentaion/Running/View/Running/PrepareRunView.swift
@@ -0,0 +1,106 @@
+//
+//  PrepareRunView.swift
+//  GatheRunner
+//
+//  Created by cho on 2022/07/09.
+//
+
+import SwiftUI
+
+// MARK: - PrepareRunView
+
+struct PrepareRunView: View {
+    var body: some View {
+        ZStack {
+            MapView(viewModel: DependencyContainer.previewMapScene).hide(isPresentedRunGuideDetailDescriptionView)
+            VStack {
+                workoutIndexView.didSetLoadable(by: $viewModel.fetchStatus)
+                RunGuideTabView(isPresentedRunGuideDetailDescriptionView: $isPresentedRunGuideDetailDescriptionView).padding(0)
+                Spacer()
+                startButton.padding(.bottom, Size.Start.padding)
+            }
+        }
+    }
+    
+    @EnvironmentObject var container: DependencyContainer
+    @StateObject var viewModel: PrepareRunViewModel
+    
+    @State private var isPresentedRunGuideDetailDescriptionView = false
+}
+
+// MARK: - NameSpace
+
+extension PrepareRunView {
+    private enum Size {
+        static let workoutIndexTextKerning: CGFloat = 2.0
+        static let workoutIndexTextLineSpacing: CGFloat = 4.0
+        static let workoutIndexTextFontSize: CGFloat = 13
+        
+        enum Start {
+            static let widthRatio = 0.2820
+            static let heightRatio = 0.1303
+            static let fontSize: CGFloat = 27
+            static let imageRadius: CGFloat = 200
+            static let padding: CGFloat = 20
+        }
+    }
+    
+    private enum Label {
+        enum Start {
+            static let title = "시작"
+        }
+    }
+}
+
+// MARK: - subViews
+
+extension PrepareRunView {
+    private var workoutIndexView: some View {
+        Rectangle()
+            .frame(height: UIScreen.getHeightby(ratio: 0.03))
+            .foregroundColor(.white)
+            .overlay {
+                Text(viewModel.todayInfo)
+                    .bold()
+                    .kerning(Size.workoutIndexTextKerning)
+                    .lineSpacing(Size.workoutIndexTextLineSpacing)
+                    .font(.system(size: Size.workoutIndexTextFontSize))
+            }
+    }
+    
+    private var startButton: some View {
+        NavigationLink(destination: runningRecord) {
+            startImage
+        }
+    }
+    
+    private var startImage: some View {
+        Circle()
+            .frame(
+                width: UIScreen.getWidthby(ratio: Size.Start.widthRatio),
+                height: UIScreen.getHeightby(ratio: Size.Start.heightRatio))
+            .overlay { startTitle }
+            .foregroundColor(.yellow)
+            .cornerRadius(Size.Start.imageRadius)
+    }
+    
+    private var startTitle: some View {
+        Text(Label.Start.title)
+            .font(.system(size: Size.Start.fontSize, weight: .black))
+            .foregroundColor(Color.black)
+    }
+    
+    private var runningRecord: some View {
+        RunningRecordView(
+            recordVm: container.viewModels.recordVm,
+            routeVm: container.viewModels.routeVm)
+    }
+}
+
+// MARK: - JustStartView_Previews
+
+struct PrepareRunView_Previews: PreviewProvider {
+    static var previews: some View {
+        PrepareRunView(viewModel: DependencyContainer.previewPrepareScene)
+    }
+}
--- GatheRunner/Presentaion/Running/ViewModel/PrepareRunViewModel.swift
+++ GatheRunner/Presentaion/Running/ViewModel/PrepareRunViewModel.swift
@@ -10,23 +10,41 @@ import Combine
 class PrepareRunViewModel: ObservableObject {
     @Published var todayInfo = ""
     @Published var fetchStatus: FetchStatus = .none
-
+    
     var errorMessage = ""
-
+    
     // MARK: Private
     
     private let workoutIndexRepository: WorkoutIndexRepository
     private let fetchStatusSubject = CurrentValueSubject<FetchStatus, Never>(.none)
     
     private var cancelBag = Set<AnyCancellable>()
-
+    
     // MARK: LifeCycle
-
+    
     init(workoutIndexRepository: WorkoutIndexRepository) {
         self.workoutIndexRepository = workoutIndexRepository
         
         bindFetchStatus()
         fetchTodayInfo()
+    }
+}
+
+// MARK: NameSpace
+
+extension PrepareRunViewModel {
+    private enum Content {
+        static let sunnyEmoji = " ☀️☀️ "
+        static let rainyEmoji = " ☁️☁️ "
+        static let gradeComment = "오늘의 운동 지수 : "
+        static let standard = "70"
+        static let comma = ","
+    }
+    
+    private enum Index {
+        static let city = 0
+        static let grade = 0
+        static let comment = 1
     }
 }
 
@@ -44,7 +62,7 @@ extension PrepareRunViewModel {
         workoutIndexRepository.fetch(TodayWorkoutIndex())
             .sink { [weak self] completion in
                 guard let self = self else { return }
-
+                
                 switch completion {
                 case .failure(let error):
                     self.fetchStatusSubject.send(.failure)
@@ -61,16 +79,20 @@ extension PrepareRunViewModel {
             }
             .store(in: &cancelBag)
     }
-
+    
     private func workoutIndexDTO(with workoutIndex: WorkoutIndex) -> String {
         
-        // MARK: Temp City Info
+        // MARK: Temp CityIndex
         
         let results = workoutIndex.outdoorGrade
-        let content = results[0].components(separatedBy: ",")
-        let grade = "오늘의 운동 지수 : " + content[0] + (content[0] >= "70" ? " ☀️☀️ " : " ☁️☁️ ")
-        let coment = content[1]
-
+        let content = results[Index.city].components(separatedBy: Content.comma)
+        
+        let grade = Content.gradeComment
+        + content[Index.grade]
+        + (content[Index.grade] >= Content.standard ? Content.sunnyEmoji : Content.rainyEmoji)
+        
+        let coment = content[Index.comment]
+        
         return grade + coment
     }
 }
